702b5f666c350288b929ccf2d30234ff9b401e4c
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-22 19:16:29 +0530

    Auto-sync: 2025-06-22 19:16:29

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+import setuptools 
 
 with open("README.md","r",encoding = "utf-8") as f:
     long_description = f.read()
@@ -10,3 +10,27 @@ REPO_NAME = "DsThakurRawat-AIML-IN-PRODUCTION-1-.-teXti"
 AUTHOR_USER_NAME = "DsThakurRawat"
 SRC_REPO = "textsummarizer"
 AUTHOR_EMAIL =  "divyanshrawatofficial@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python package for NLP-based text summarization using Hugging Face Transformers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[]  # or list your dependencies here
+)
