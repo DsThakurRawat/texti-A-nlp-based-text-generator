27ff3341a9a6f4375aa412f29a78e56ee3840ddc
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-22 19:10:28 +0530

    Auto-sync: 2025-06-22 19:10:28

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md","r",encoding = "utf-8") as f:
+    long_description = f.read()
+
+
+_version_= "0.0.0"
+
+REPO_NAME = "DsThakurRawat-AIML-IN-PRODUCTION-1-.-teXti"
+AUTHOR_USER_NAME = "DsThakurRawat"
+SRC_REPO = "textsummarizer"
+AUTHOR_EMAIL "divyanshrawatofficial@gmail.com"
