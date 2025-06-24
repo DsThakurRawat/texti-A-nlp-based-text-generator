519d300934fbf069421e5adfc5e3a47650aa8372
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-24 15:23:24 +0530

    Auto-sync: 2025-06-24 15:23:24

--- src/textsummarizer/entity/__init__.py
+++ src/textsummarizer/entity/__init__.py
@@ -1,5 +1,3 @@
-# filepath: src/textsummarizer/entity/__init__.py
-
 from .data_ingestion_config import DataIngestionConfig
 
 from dataclasses import dataclass
@@ -8,17 +6,5 @@ from pathlib import Path
 @dataclass(frozen=True)
 class DataValidationConfig:
     root_dir: Path
-    source_url: str
-    local_data_file: Path
-    unzip_dir: Path
-
-
-
-
-@dataclass(frozen=True) 
-class DataValidationconfig:
-    root_dir: Path
     STATUS_FILE: str
     ALL_REQUIRED_FILES: list
-    
-
