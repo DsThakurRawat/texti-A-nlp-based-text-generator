dca793fa3acee32f7ba40edb09142dbaeda27c46
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-24 10:20:36 +0530

    Auto-sync: 2025-06-24 10:20:36

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from textsummarizer.pipeline.stage_01_data_ingestion import  DataIngestionTrainingPipeline
+
+from textsummarizer.logging.logger import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_ingestion_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x\n\n")
+except Exception as e:
+    logger.exception(f">>>>>>>>>> stage {STAGE_NAME} failed <<<<<<<<")
+    raise e
+
