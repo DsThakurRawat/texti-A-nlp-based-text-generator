3e10ef9bdbc1a7c525e6fb1ef69887aaf088cf33
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-24 13:56:07 +0530

    Auto-sync: 2025-06-24 13:56:07

--- src/main.py
+++ src/main.py
@@ -1,13 +1,25 @@
-from textsummarizer.pipeline.stage_01_data_ingestion import  DataIngestionTrainingPipeline
-
+from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textsummarizer.logging.logger import logger
 
+# Data Ingestion Stage
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
-    data_ingestion_pipeline = DataIngestionTrainingPipeline()
-    data_ingestion_pipeline.main()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x\n\n")
 except Exception as e:
-    logger.exception(f">>>>>>>>>> stage {STAGE_NAME} failed <<<<<<<<")
+    logger.exception(e)
+    raise e
+
+# Data Validation Stage
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\nx==========x\n\n")
+except Exception as e:
+    logger.exception(e)
     raise e
