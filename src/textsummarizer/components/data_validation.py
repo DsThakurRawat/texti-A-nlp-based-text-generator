cf0c9974f2805aab7a8f76478226856ef4dca01b
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-24 13:43:38 +0530

    Auto-sync: 2025-06-24 13:43:38

--- src/textsummarizer/components/data_validation.py
+++ src/textsummarizer/components/data_validation.py
@@ -4,14 +4,25 @@ from textsummarizer.entity import DataIngestionConfig
 from textsummarizer.entity import DataValidationConfig
 
 class DataValidation:
-    def __init__(self, config : DataValidationConfig):
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+    def validate_all_files_exist(self) -> bool:
         try:
             validation_status = None
-            all_files = os.listdir(os.path.join("artifacts","data_ingestion","samsum_dataset"))
+            all_files = os.listdir(os.path.join("artifacts", "data_ingestion", "samsum_dataset"))
 
             for file in all_files:
                 if file not in self.config.ALL_REQUIRED_FILES:
                     validation_status = False
-                    logger.error(f"Missing required file: {file}")
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        f.write(f"validation status: {validation_status}\n")
                 else:
                     validation_status = True
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        f.write(f"validation status: {validation_status}\n")
+
+            return validation_status
+        except Exception as e:
+            logger.error(f"Error occurred during data validation: {e}")
+            raise e
