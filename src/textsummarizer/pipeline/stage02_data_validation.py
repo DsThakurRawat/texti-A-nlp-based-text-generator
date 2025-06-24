756f6e95c9ab2cec90d76868494037ad8d021620
DsThakurRawat <DsThakurRawat@users.noreply.github.com>
2025-06-24 13:47:02 +0530

    Auto-sync: 2025-06-24 13:47:02

--- src/textsummarizer/pipeline/stage02_data_validation.py
+++ src/textsummarizer/pipeline/stage02_data_validation.py
@@ -1,5 +1,5 @@
 from textsummarizer.config.configuration import ConfigurationManager
-from textsummarizer.components.data_validation import DataIngestion
+from textsummarizer.components.data_validation import DataValidation
 from textsummarizer.logging.logger import logger
 
 class DataValidationTrainingPipeline:
@@ -8,7 +8,7 @@ class DataValidationTrainingPipeline:
     def main(self):
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion = DataValidation(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
     
